4a3058a5d64e5f823659392bc717b5bb5045d91c
andre <andrey.kozlovskiy.v@gmail.com>
2026-04-01 14:42:44 +0200

    fgixed 11

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -36,10 +36,9 @@ const stats = computed(() => [
 
 onMounted(() => {
   const prefersReducedMotion = window.matchMedia('(prefers-reduced-motion: reduce)').matches
-  const isCoarsePointer = window.matchMedia('(hover: none) and (pointer: coarse)').matches
   const saveData = (navigator as Navigator & { connection?: { saveData?: boolean } }).connection?.saveData === true
 
-  useVideo.value = !(prefersReducedMotion || saveData || isCoarsePointer)
+  useVideo.value = !(prefersReducedMotion || saveData)
 
   onVisibilityChange = () => {
     const video = heroVideoRef.value
